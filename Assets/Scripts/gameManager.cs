24cd18c37475d1aba24dfdb143fd3725cf210ba7
davi torres <davi_tl@outlook.com>
2026-04-19 11:42:21 -0400

    Audio Menu without audios

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -12,7 +12,7 @@ public class gamemanager : MonoBehaviour
     [SerializeField] GameObject menuPause;
     [SerializeField] GameObject menuWin;
     [SerializeField] GameObject menuLose;
-
+    [SerializeField] GameObject menuAudio;
 
     public Image playerHPbar;
     public GameObject player;
@@ -115,6 +115,23 @@ public class gamemanager : MonoBehaviour
         statePause();
         menuActive = menuLose;
         menuActive.SetActive(true);
+    }
+
+    public void openAudio()
+    {
+        if (menuActive != null)
+            menuActive.SetActive(false);
+
+        menuActive = menuAudio;
+        menuAudio.SetActive(true);
+    }
+
+    public void backToPause()
+    {
+        menuAudio.SetActive(false);
+
+        menuActive = menuPause;
+        menuPause.SetActive(true);
     }
 
 }
