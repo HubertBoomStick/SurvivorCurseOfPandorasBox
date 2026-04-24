c3da917cb6c15497376e1f33ee3c0abf7ad73285
TK <TKXiong@student.fullsail.edu>
2026-04-24 15:00:30 -0400

    Revert "Merge branch 'TK-Feature' into Dev"
    This reverts commit c2299e0cb806a57403f6fdf8df184c089b97c1ea, reversing
    changes made to 51218a15da21a3663bd91a46a35f66ec1f394a0f.

--- Assets/Scripts/Boss Enemy/DragonBreath.cs
+++ Assets/Scripts/Boss Enemy/DragonBreath.cs
@@ -1,39 +1,59 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DragonBreath : MonoBehaviour
 {
     [Header("Damage Settings")]
-    [SerializeField] private int damage = 1;
-    [SerializeField] private float tickRate = 0.75f;
+    [SerializeField] private int damage = 18;
+    [SerializeField] private float tickRate = 0.25f;
     [SerializeField] private LayerMask hitLayers;
 
-    private readonly Dictionary<Collider, float> damageTimers = new Dictionary<Collider, float>();
+    private float tickTimer;
+    private BoxCollider breathCollider;
 
-    public void SetUp(int newDamage, LayerMask newHitLayers)
+    public void SetUp(int newDamage, float lifeTime, LayerMask newHitLayers)
     {
         damage = newDamage;
         hitLayers = newHitLayers;
-        damageTimers.Clear();
+
+        Destroy(gameObject, lifeTime);
     }
 
-    private void OnTriggerStay(Collider other)
+    private void Awake()
     {
-        if (((1 << other.gameObject.layer) & hitLayers) == 0)
+        breathCollider = GetComponent<BoxCollider>();
+    }
+
+    private void Update()
+    {
+        tickTimer += Time.deltaTime;
+
+        if (tickTimer >= tickRate)
+        {
+            tickTimer = 0f;
+            DealBreathDamage();
+        }
+    }
+
+    private void DealBreathDamage()
+    {
+        if (breathCollider == null)
             return;
 
-        if (!damageTimers.ContainsKey(other))
+        Vector3 worldCenter = breathCollider.transform.TransformPoint(breathCollider.center);
+        Vector3 halfExtents = Vector3.Scale(breathCollider.size * 0.5f, breathCollider.transform.lossyScale);
+        Quaternion rotation = breathCollider.transform.rotation;
+
+        Collider[] hits = Physics.OverlapBox(
+            worldCenter,
+            halfExtents,
+            rotation,
+            hitLayers
+        );
+
+        for (int i = 0; i < hits.Length; i++)
         {
-            damageTimers[other] = 0f;
-        }
+            IDamage damageable = hits[i].GetComponent<IDamage>();
 
-        damageTimers[other] += Time.deltaTime;
-
-        if (damageTimers[other] >= tickRate)
-        {
-            damageTimers[other] = 0f;
-
-            IDamage damageable = other.GetComponent<IDamage>();
             if (damageable != null)
             {
                 damageable.takeDamage(damage);
@@ -41,16 +61,18 @@ public class DragonBreath : MonoBehaviour
         }
     }
 
-    private void OnTriggerExit(Collider other)
+    private void OnDrawGizmosSelected()
     {
-        if (damageTimers.ContainsKey(other))
-        {
-            damageTimers.Remove(other);
-        }
-    }
+        BoxCollider box = GetComponent<BoxCollider>();
 
-    private void OnDisable()
-    {
-        damageTimers.Clear();
+        if (box == null)
+            return;
+
+        Gizmos.color = Color.magenta;
+
+        Matrix4x4 oldMatrix = Gizmos.matrix;
+        Gizmos.matrix = box.transform.localToWorldMatrix;
+        Gizmos.DrawWireCube(box.center, box.size);
+        Gizmos.matrix = oldMatrix;
     }
 }
